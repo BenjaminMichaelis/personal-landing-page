e232322571776366045ac5c1142c7a77cce9d7c6
Benjamin Michaelis <benjamin@michaelis.net>
2022-02-04 23:27:26 -0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ import '../css/components.css'
 function Footer() {
     return (
         <div className="Footer container">
-            <p>Made with <span class="heart">❤️</span> by <a href="https://github.com/BenjaminMichaelis target="_blank" rel="noopener noreferrer">Benjamin Michaelis</a></p>
+            <p>Made with <span class="heart">❤️</span> by <a href="https://github.com/BenjaminMichaelis" target="_blank" rel="noopener noreferrer">Benjamin Michaelis</a></p>
             <a href="https://github.com/BenjaminMichaelis/personal-landing-page" target="_blank" rel="noopener noreferrer">
                     <div class="link">
                         <i class="fab fa-github fa-2x"></i>
